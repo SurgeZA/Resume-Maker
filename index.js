3f6de1119b4c4e4078adde674f852021b3361461
SurgeZA <Scottlogandennis@gmail.com>
2022-02-22 15:39:38 +0200

    pdf

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-// const { Router } = require('express');
+
 const express = require('express');
+const pdf = require('html-pdf')
 const expressLayouts = require('express-ejs-layouts');
+const dynamicCV = require("./docs/dynamic-resume");;
+
 // const res = require('express/lib/response');
 const app = express();
 
@@ -8,10 +11,20 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+const options = {
+    "height": "10.5in",
+    "width": "8in",
+};
 
 app.get('/', (req, res, next) => {
     res.render('home');
 });
+
+
 
 app.get('/cv-generator/:theme', (req, res, next) => {
     console.log("theme: ", req.params.theme);
@@ -30,6 +43,23 @@ app.get('/cv-generator/:theme', (req, res, next) => {
     res.render('cv-generator', { theme: req.params.theme });
 });
 
+app.post('/cv-generator', (req, res, next) => {
+    console.log(req.body);
+
+    const userName = req.body.name;
+    const lowercaseName = userName.toLowerCase();
+    const noSpaceName = lowercaseName.replace(' ', '');
+    const shortName = noSpaceName.slice(0, 12);
+    console.log("short name: ", shortName);
+
+
+
+    pdf.create(dynamicCV(), options).toFile(__dirname + "/docs/dynamic-resume.pdf", (error, response) => {
+        if (error) throw Error("File is not created");
+        console.log(response.filename);
+        res.sendFile(response.filename);
+    });
+});
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log('Server is running on: ' + port))
+app.listen(port, () => console.log('Server is running on: ' + port));
